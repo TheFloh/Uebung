ff8d8356954350899dd686d66efbb30416b7b6c5
Huber.Florian <Huber.Florian@SK-D8CB8A4FAAFF>
2015-10-02 09:05:59 +0200

    Operatoren Rightorfalse

--- Uebung/src/Uebung.java
+++ Uebung/src/Uebung.java
@@ -4,6 +4,16 @@ public class Uebung {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	System.out.println(Wahroderfalsch());
+	
+	}
+		
+		static boolean Wahroderfalsch(){
+		return (4==7 && 5==3);
+	}
+	
+	
+	
 	}
 
-}
+
