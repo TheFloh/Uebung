f27caa78abb20e31cea4b0f66280bbbd5417e9ac
Huber.Florian <Huber.Florian@SK-D8CB8A4FAAFF>
2015-10-09 09:14:01 +0200

    collatz

--- Uebung/src/Uebung.java
+++ Uebung/src/Uebung.java
@@ -1,19 +1,45 @@
 
 public class Uebung {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	System.out.println(Wahroderfalsch());
 	
+	static int fac(int i){ 
+		return i==0?1:i*fac(i-1);
 	}
 		
-		static boolean Wahroderfalsch(){
-		return (4==7 && 5==3);
+public static void main(String[] args) {
+	System.out.println("fac(2) = "+fac(2));
+	System.out.println(fibonacci(8));}
+
+	
+static void fibfolge(long max){
+	for(long i =0; i<=max;i++){
+		if(i==max){System.out.println(fibonacci(i));}
+		else{System.out.print(fibonacci(i)+", ");}
 	}
-	
-	
-	
+}	
+
+static long fibonacci(long i){
+	if(i<=0){return 0;}
+	else {
+		if(i==1 || i==2){return 1;}
+		else {
+			return(fibonacci(i-2)+fibonacci(i-1));}
+		
+		}
 	}
 
+static long collatz (long a);{
+	if a
+}
+
+
+}
+
+
+
+
+	
+	
+	
 
