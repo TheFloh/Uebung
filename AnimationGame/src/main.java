063097b7017932a6b4264f83d0011fbc6cc0e44a
Huber.Florian <Huber.Florian@SK-D8CB8A4FAAFF>
2015-10-16 09:15:16 +0200

    adf

--- AnimationGame/src/main.java
+++ AnimationGame/src/main.java
@@ -5,9 +5,46 @@ public class main {
 		
 		SimpleOval s1= new SimpleOval (45,53);
 		SimpleOval s2=new SimpleOval (45,53);
-		
+		Square sq1=new Square(5, new Vertex(50,50));
+		Square sq2=new Square(5, new Vertex(50,50));		
 		System.out.println(s1.equals(s2));
 		System.out.println(s1);
+	
+		 /* Vertex v1 = new Vertex(42,53);
+		   Vertex v2 = new Vertex(32,4);
+		   System.out.println(v1.distance(v2));
+		   v2.normalize();
+		   System.out.println(v2.toString());
+		   System.out.println(v1.length()); 
+		   
+		GeometricObject o1 = new GeometricObject(5);
+		System.out.println(o1);
+		
+		GeometricObject o1 = new GeometricObject(17,4, new Vertex(42,23));
+		GeometricObject o2 = new GeometricObject(17,4);
+		GeometricObject o3 = new GeometricObject(17);
+		GeometricObject o4 = new GeometricObject();
+		
+		System.out.println(o1.equals(o2));
+		o2.moveTo(42,23);
+		System.out.println(o1.equals(o2));
+		System.out.println(o3.contains(new Vertex(10,3)));
+		System.out.println(o4); */
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	}
 
 
