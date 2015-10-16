b3ea2134deb55612fcae17dafe4240751f4c3926
Huber.Florian <Huber.Florian@SK-D8CB8A4FAAFF>
2015-10-16 08:27:38 +0200

    xs

---
+++ AnimationGame/src/SimpleOval.java
@@ -0,0 +1,17 @@
+
+public class SimpleOval extends GeometricObject {
+	
+	public SimpleOval (double w, double h, Vertex pos){
+		super(w, h, pos);
+	}
+	
+	public SimpleOval (double w, double h, double x, double y){
+		super(w,h, new Vertex(x,y));
+	}
+	
+	@Override public double area(){return Math.PI*width*height/4;}
+	
+	@Override public String toString(){
+		return "SimpleOval("+super.toString()+")";}
+	}
+
