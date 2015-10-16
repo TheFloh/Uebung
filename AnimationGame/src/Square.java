3927222314e20d0b3c348dc795b67292df865964
Huber.Florian <Huber.Florian@SK-D8CB8A4FAAFF>
2015-10-16 09:02:54 +0200

    Bugfix

--- AnimationGame/src/Square.java
+++ AnimationGame/src/Square.java
@@ -1,12 +1,12 @@
 
 public class Square extends GeometricObject {
 	
-	public Square (double w, double h, Vertex pos){
-		super(w, h, pos);
+	public Square (double w, Vertex pos){
+		super(w, w, pos);
 	}
 
-	public Square (double w, double h, double y){
-		super(w,h, new Vertex(0,0));
+	public Square (double w, double y){
+		super(w,w, new Vertex(0,0));
 	}
 	
 	@Override public double area(){return width*width;}
