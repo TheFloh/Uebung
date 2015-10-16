923515f4428c6a27d326c903b8b6246f2f20425c
Huber.Florian <Huber.Florian@SK-D8CB8A4FAAFF>
2015-10-16 08:56:07 +0200

    s

---
+++ AnimationGame/src/EquilateralTriangle.java
@@ -0,0 +1,17 @@
+
+public class EquilateralTriangle extends GeometricObject{
+	
+	public EquilateralTriangle (double w, double h, Vertex pos){
+		super(w, h, pos);}
+
+	public EquilateralTriangle (double w, double h, double x, double y){
+		super(w,h, new Vertex(x,y));}
+	
+	@Override public double area(){return (width*height)/2;}
+	
+	@Override public String toString(){
+			return "EquilateralTriangle("+super.toString()+")";}
+	
+	@Override public boolean equals(Object that){
+		return(that instanceof SimpleOval && super.equals(that));}
+}
