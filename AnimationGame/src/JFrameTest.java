8e699253199c0f062677450991ce9f7cb1371f98
Huber.Florian <Huber.Florian@SK-D8CB8A4FAAFF>
2015-10-20 14:42:42 +0200

    JFrame

---
+++ AnimationGame/src/JFrameTest.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+public class JFrameTest {
+	
+	public static void main(String[] args){
+		JFrame f=new JFrame("Hier könnte Ihre Werbung stehen");
+		f.setVisible(true);
+	
+	
+	}
+	
+	
+}
