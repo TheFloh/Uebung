2f0ed4849bb213accde857cb09db0af8a1067ca3
Huber.Florian <Huber.Florian@SK-D8CB8A4FAAFF>
2015-10-23 08:51:07 +0200

    bugfix

--- AnimationGame/src/SimplePaintableRectangle.java
+++ AnimationGame/src/SimplePaintableRectangle.java
@@ -1,11 +1,11 @@
 
 public class SimplePaintableRectangle extends SimpleRectangle implements Paintable {
 	
-public SimplePaintableRectangle(double width, double height){
-	super(width, height);}
+public SimplePaintableRectangle(double width, double height,double x, double y){
+	super(width, height,new Vertex(x,y));}
 
 public void paintTo(java.awt.Graphics g){
-	g.fillRect((int)width, (int)height, 0, 0);}
+	g.fillRect((int)pos.x, (int)pos.y, (int)width, (int)height);}
 }
 
 
