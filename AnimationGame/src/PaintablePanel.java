e3d48fcefca1558036b06169b7eb4713389e042b
Huber.Florian <Huber.Florian@SK-D8CB8A4FAAFF>
2015-10-27 15:10:57 +0100

    commit

--- AnimationGame/src/PaintablePanel.java
+++ AnimationGame/src/PaintablePanel.java
@@ -1,22 +1,17 @@
 import java.awt.Color;
+
 public class PaintablePanel extends SizedPanel{
 
 	Paintable pa;
-	
 	public PaintablePanel(Paintable pa){this.pa=pa;}
 	
-	public void PaintComponent(java.awt.Graphics g){
+	public void paintComponent(java.awt.Graphics g){
 		pa.paintTo(g);
 	}
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		ShowInFrame.show (new PaintablePanel(new PaintableOval(100,50,30,50,new Color(23,23,23))));
-		ShowInFrame.show (new PaintablePanel(new PaintableOval(10,10,130,40,new Color(59,34,65))));
-		ShowInFrame.show(new PaintablePanel(new SimplePaintableRectangle(45,34,54,43,new Color(45,32,76,88))));
-	
+		public static void main(String [] args){
+			ShowInFrame.show (new PaintablePanel(new PaintableOval(100,50,30,50,new Color(23,23,23))));
+			ShowInFrame.show (new PaintablePanel(new PaintableOval(10,10,130,40,new Color(59,34,65))));
+			ShowInFrame.show (new PaintablePanel(new PaintableStar(new Vertex(50,50), new Color(59,34,65))));
+			
+		}
 	}
-	
-
-}
